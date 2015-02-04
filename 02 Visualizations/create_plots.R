05d7f05c3e2cd5ac094060e83aed6d71c6a6a370
NikolajPM <nikolajplagborg@gmail.com>
2015-02-04 16:29:50 -0600

    Almost done

--- 02 Visualizations/create_plots.R
+++ 02 Visualizations/create_plots.R
@@ -6,7 +6,21 @@ dfod <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.14
 
 dfi <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from items"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-head(dfc)
-head(dfi)
-head(dfo)
-head(dfod)
+mega_set <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from orders o join customers c on o.customer_id = c.customer_id join order_details d on o.order_id = d.order_id join items i on d.item_id = i.item_id"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal', USER='DV_ORDERS', PASS='orcl', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+
+ggplot(dfc, aes(x=CUSTOMER_STATE, y= CUSTOMER_CITY)) + geom_point()
+
+ggplot(dfi, aes(x=ARTIST, y=UNIT_PRICE)) + geom_point()
+
+ggplot(dfo, aes(x=ORDER_DATE, y=CUSTOMER_ID)) + geom_point()
+
+ggplot(dfod, aes(x=ITEM_ID, y= ORDER_QTY)) + geom_point()
+
+p1 <- ggplot(mega_set, aes(x=as.Date(ORDER_DATE, "%Y-%m-%d"), y=as.Date(SHIPPED_DATE, "%Y-%m-%d"), color = as.factor(UNIT_PRICE))) + geom_point()
+p1 + facet_wrap(~CUSTOMER_STATE)
+
+p2 <- ggplot(mega_set, aes(x=as.Date(ORDER_DATE, "%Y-%m-%d"), y=as.Date(SHIPPED_DATE, "%Y-%m-%d"), color = as.factor(TITLE))) + geom_point()
+p2 + facet_wrap(~CUSTOMER_STATE)
+
+p3 <- ggplot(mega_set, aes(x=as.Date(ORDER_DATE, "%Y-%m-%d"), y=as.Date(SHIPPED_DATE, "%Y-%m-%d"), color = as.factor(ARTIST))) + geom_point()
+p3 + facet_wrap(~CUSTOMER_STATE)
